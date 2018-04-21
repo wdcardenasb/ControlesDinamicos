4cbc90259cd31b9ee1590e5cb56d47c14eb0a04f
wdcardenasb <38057700+wdcardenasb@users.noreply.github.com>
2018-04-21 14:15:41 -0500

    CommitTriger

---
+++ ControlesDinamicos/ControlesDinamicos/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ControlesDinamicos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BotonUno.Clicked += BotonUno_Clicked;
+        }
+
+        private void BotonUno_Clicked(object sender, EventArgs e)
+        {
+            Panel.Children.Add(new Button()
+            {
+                Text=$"Boton {Panel.Children.Count+1}"
+            });
+        }
+    }
+}
